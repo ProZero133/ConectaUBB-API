1d5a77b5caa497575e5642cae43bc6a1bbe5fbd6
Sebastian Vallejos <sebastian.vallejos2001@alumnos.ubiobio.cl>
2024-12-20 17:22:01 -0300

    Fix conexion con el servidor para la vista con sql server

--- Config/db.js
+++ Config/db.js
@@ -1,20 +1,34 @@
-const { Pool } = require("pg");
+const sql = require("mssql");
 const configEnv = require('../Config/env.config.js');
 const userDB = configEnv.userDB;
 const passwordDB = configEnv.passwordDB;
 const serverIP = configEnv.serverIP;
-const portDB = configEnv.portDB;
+const portDB = parseInt(configEnv.portDB);
 const database = configEnv.database;
-dotenv.config();
 
-const pool = new Pool({
+const config = {
   user: userDB,
   password: passwordDB,
-  host: serverIP,
+  server: serverIP,
   port: portDB,
   database: database,
-});
-pool.query('SELECT 1')
-    .then(() => console.log('Conexión a la base de datos exitosa'))
-    .catch(err => console.error('Error al conectar a la base de datos:', err));
-module.exports = { pool };
+  options: {
+    encrypt: true, // Use this if you're on Windows Azure
+    trustServerCertificate: true // Change to true for local dev / self-signed certs
+  }
+};
+
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log('Connected to SQL Server');
+    return pool;
+  })
+  .catch(err => {
+    console.error('Database Connection Failed! Bad Config: ', err);
+    throw err;
+  });
+
+module.exports = {
+  sql, poolPromise
+};
--- services/user.service.js
+++ services/user.service.js
@@ -1,10 +1,16 @@
+const { poolPromise } = require('../Config/db.js');
+
 async function ObtenerUsuarios(req, res) {
     try {
-        console.log('ObtenerUsuarios');
+        const pool = await poolPromise;
+        // Query de ejemplo donde nombre sea igual a Sebastian
+        const result = await pool.request().query('SELECT * FROM VTA_WEB_IDENTIFICACION_API WHERE correo = \'vicente.castillo2001@alumnos.ubiobio.cl\'');
+        console.log(result);
     } catch (error) {
-        res.status(500).json({ error: error.message });   
+        res.status(500).send({ error: error.message });
     }
 }
+
 module.exports = {
-ObtenerUsuarios
+    ObtenerUsuarios
 };
